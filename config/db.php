c50b6e95f8368a709207c2ff4fd7aab23d456d9d
andy <andruha2921@gmail.com>
2016-02-03 17:22:04 +0200

    Prepeir for dev

--- config/db.php
+++ config/db.php
@@ -1,16 +1,17 @@
 <?php
 
-return [
+return defined('YII_ENV') && YII_ENV == 'dev'?
+    [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=taskmanager',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-];
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=taskmanager',
-//    'username' => 'root',
-//    'password' => '',
-//    'charset' => 'utf8',
-//];
+    ] :
+    [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=mysql.hostinger.com.ua;dbname=u816997872_tm',
+        'username' => 'u816997872_tm',
+        'password' => 'GxgcflM9cR',
+        'charset' => 'utf8',
+    ];
